cc928ca4ba175d64e34aef42172dca979587ec2f
okook659 <araulbora@gmail.com>
2025-03-23 17:44:23 +0000

    fonctionnalite de connexion

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     //Pour protéger vos routes, il suffit de mettre les routes ici
-    Route::get('/', function () {
-        return view('welcome');
-    });
+    // Route::get('/', function () {
+    //     return view('welcome');
+    // });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
